ef0cc5d35665565cc264cac2ed0ff0bf58ec654b
Martins Balodis <martins256@gmail.com>
2017-11-15 12:22:26 +0200

    fix timestamps aren't available in the migration

--- src/ScrapedRecord.php
+++ src/ScrapedRecord.php
@@ -4,28 +4,11 @@ namespace WebScraper\LaravelDataImporter;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * App\ScrapedRecord
- *
- * @mixin \Eloquent
- * @property int $id
- * @property int $scrapingjob_id
- * @property int $sitemap_id
- * @property string $sitemap_name
- * @property string $web_scraper_order
- * @property string $data
- * @property int $time_created
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereData($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereScrapingjobId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereSitemapId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereSitemapName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereTimeCreated($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\WebScraper\LaravelDataImporter\ScrapedRecord whereWebScraperOrder($value)
- */
 class ScrapedRecord extends Model {
 
 	protected $table = "scraped_records";
+
+	public $timestamps = false;
 
 	protected $casts = [
 		'data' => 'array',
